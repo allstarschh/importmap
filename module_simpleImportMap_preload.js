b5728709efb42b5a5d217467d334ea15e6b48cbe
Yoshi Cheng-Hao Huang <allstars.chh@gmail.com>
2022-05-25 10:28:43 +0200

    try modulepreload

---
+++ module_simpleImportMap_preload.js
@@ -0,0 +1,5 @@
+//import { x } from "simple";
+import { x } from "./module_simpleExport.js";
+console.log("module_simpleImportMap.mjs result="+result);
+result = x;
+console.log("module_simpleImportMap.mjs after result="+result);
